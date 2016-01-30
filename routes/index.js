b3933a27788bd0deae76f699be0ddcc7f88f5a98
Brian Smith <bds6052@gmail.com>
2016-01-30 22:58:47 +0000

    Fix issues on new home page

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-    res.render('main_index', {title: 'Golf Routines'});
+router.get('/', function(req, res) {
+    res.render('main_index.jade');
 });
 
 router.get('/golfCourseMap', function(req, res) {
-    res.render('golfCourseMap');
+    res.render('golfCourseMap.jade');
 });
 
 //router.get('/gis', function(req, res){
