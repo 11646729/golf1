413cc3d3f56888d8d6bb0f0ea5a75e8930de55ee
Brian Smith <bds6052@gmail.com>
2017-04-24 10:33:35 +0100

    Corrected addGCCalendar js & hbs files & minor refactored hbs files

--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,9 @@ module.exports = function (io) {
   var user = require('../passport/user')
   var gcal = require('google-calendar')
 
+  // Not yet used - reformat date returned from google calendar
+  var myUtil = require('../static/javascripts/calendarUtilities')
+
   var connection
 
   var router = express.Router()
@@ -18,7 +21,8 @@ module.exports = function (io) {
    * Home Page
    */
   router.get(config.routes.home, function (req, res) {
-    res.render('home', { title: 'Golf Test Routines' })
+    var data = {title: 'Golf Test Routines'}
+    res.render('home', data)
   })
 
   /**
@@ -26,7 +30,8 @@ module.exports = function (io) {
    */
   router.get(config.routes.register, function (req, res) {
     // Missing message error
-    res.render('register', { title: 'Golf Test Routines' })
+    var data = {title: 'Golf Test Routines'}
+    res.render('register', data)
   })
 
   /**
@@ -55,7 +60,8 @@ module.exports = function (io) {
    */
   router.get(config.routes.login, function (req, res) {
     // Missing message error
-    res.render('login', { title: 'Golf Test Routines' })
+    var data = {title: 'Golf Test Routines'}
+    res.render('login', data)
   })
 
   /**
@@ -70,28 +76,36 @@ module.exports = function (io) {
    * About
    */
   router.get(config.routes.about, [util.requireAuthentication], function (req, res) {
-    res.render('about', { title: 'Golf Test Routines', pageName: 'About Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'About Page'}
+    res.render('about', data)
   })
 
   /**
    * Contact Page
    */
   router.get(config.routes.contact, [util.requireAuthentication], function (req, res) {
-    res.render('contact', { title: 'Golf Test Routines', pageName: 'Contact Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Contact Page'}
+    res.render('contact', data)
   })
 
   /**
    * Main Page
    */
   router.get(config.routes.mainPage, [util.requireAuthentication], function (req, res) {
-    res.render('main', { title: 'Golf Test Routines' })
+
+    var data = {title: 'Golf Test Routines'}
+    res.render('main', data)
   })
 
   /*
    * Get Nearby Golf Courses
    */
   router.get(config.routes.nearbyGolfCourses, [util.requireAuthentication], function (req, res) {
-    res.render('nearbyGolfCourses', { title: 'Golf Test Routines', pageName: 'Nearby Golf Courses Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Nearby Golf Courses Page'}
+    res.render('nearbyGolfCourses', data)
 
     io.on('connection', function () {
       r.connect({host: config.rethinkdb.host, port: config.rethinkdb.port}, function (err, conn) {
@@ -130,7 +144,9 @@ module.exports = function (io) {
    * Get Rounds Of Golf
    */
   router.get(config.routes.roundsOfGolf, [util.requireAuthentication], function (req, res) {
-    res.render('roundsOfGolf', { title: 'Golf Test Routines', pageName: 'Get Rounds of Golf Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Get Rounds of Golf Page'}
+    res.render('roundsOfGolf', data)
 
     io.on('connection', function () {
       r.connect({host: config.rethinkdb.host, port: config.rethinkdb.port}, function (err, conn) {
@@ -169,42 +185,54 @@ module.exports = function (io) {
    * Find All My Rounds Of Golf
    */
   router.get(config.routes.findAllMyRounds, [util.requireAuthentication], function (req, res) {
-    res.render('findAllMyRounds', { title: 'Golf Test Routines', pageName: 'Find All My Rounds Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Find All My Rounds Page'}
+    res.render('findAllMyRounds', data)
   })
 
   /**
    * Add My Round Of Golf
    */
   router.get(config.routes.addMyRound, [util.requireAuthentication], function (req, res) {
-    res.render('addMyRound', { title: 'Golf Test Routines', pageName: 'Add My Round Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Add My Round Page'}
+    res.render('addMyRound', data)
   })
 
   /**
    * Find My Round Of Golf By Id
    */
   router.get(config.routes.findMyRoundById, [util.requireAuthentication], function (req, res) {
-    res.render('findMyRoundById', { title: 'Golf Test Routines', pageName: 'Find My Round By Id Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Find My Round By Id Page'}
+    res.render('findMyRoundById', data)
   })
 
   /**
    * Delete My Round Of Golf
    */
   router.get(config.routes.deleteMyRound, [util.requireAuthentication], function (req, res) {
-    res.render('deleteMyRound', { title: 'Golf Test Routines', pageName: 'Delete My Round Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Delete My Round Page'}
+    res.render('deleteMyRound', data)
   })
 
   /**
    * Course Scorecards
    */
   router.get(config.routes.courseScorecards, [util.requireAuthentication], function (req, res) {
-    res.render('courseScorecards', { title: 'Golf Test Routines', pageName: 'Course Scorecards Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Course Scorecards Page'}
+    res.render('courseScorecards', data)
   })
 
   /**
    * Membership Relationship Manager
    */
   router.get(config.routes.membershipRelationshipManager, [util.requireAuthentication], function (req, res) {
-    res.render('membershipRelationshipManager', { title: 'Golf Test Routines', pageName: 'Membership Relationship Manager Page' })
+
+    var data = {title: 'Golf Test Routines', pageName: 'Membership Relationship Manager Page'}
+    res.render('membershipRelationshipManager', data)
   })
 
   /**
@@ -216,6 +244,7 @@ module.exports = function (io) {
         console.log('[ERROR] %s:%s \n%s', err.name, err.msg, err.message)
         res.send([])
       } else {
+
         eventList = JSON.parse(eventList)
 
         console.log(eventList) // Using this line shows that extendedProperties are not being returned
@@ -227,7 +256,7 @@ module.exports = function (io) {
 
         console.log('In the Read Competitions route')
 
-        var data = { title: 'Golf Test Routines', pageName: 'Read Competitions Page', events: events }
+        var data = {title: 'Golf Test Routines', pageName: 'Read Competitions Page', events: events}
         res.render('readCompetitions', data)
       }
     })
@@ -257,7 +286,7 @@ module.exports = function (io) {
 
         console.log('In the Read google-calendar Competitions route')
 
-        var data = { title: 'Golf Test Routines', pageName: 'Read google-calendar Competitions Page', events: events }
+        var data = {title: 'Golf Test Routines', pageName: 'Read google-calendar Competitions Page', events: events}
         res.render('readGCCompetitions', data)
       }
     })
@@ -268,90 +297,51 @@ module.exports = function (io) {
    */
   router.get(config.routes.addGCCompetitions, [util.requireAuthentication], function (req, res) {
 
-/*
     var googleCalendar = new gcal.GoogleCalendar(config.google.googleOAuth2AccessToken)
 
+    //  Sample event
     var addEventBody = {
       'status': 'confirmed',
+      'htmlLink': 'https://www.google.com/calendar/event?eid=b28zdWNyYmoyOGlwaTluYmlvYjJjYzg5dDQgYmRzNjA1MkBt',
+      'created': '2017-04-28T18:57:21.000Z',
+      'updated': '2017-04-28T18:57:21.417Z',
       'summary': 'Constructed calendar test - summary',
       'description': 'Constructed calendar test - description',
       'location': 'Clandeboye Golf Club, 51 Tower Rd, Bangor, Newtownards BT23, UK',
       'creator': {
         'email': 'bds6052@gmail.com',
         'displayName': 'Brian Smith',
-        'self': true
-      },
-      'organizer': {
-        'email': 'bds6052@gmail.com',
-        'displayName': 'Brian Smith',
-        'self': true
+        'self': true,
+        'organizer': {
+          'email': 'bds6052@gmail.com',
+          'displayName': 'Brian Smith',
+          'self': true
+        }
       },
       'start': {
-        'dateTime': '2017-03-31T20:00:00+01:00',
+        'dateTime': '2017-04-28T20:00:00+01:00',
         'timeZone': 'Europe/London'
       },
       'end': {
-        'dateTime': '2017-03-31T21:00:00+01:00',
+        'dateTime': '2017-04-28T21:00:00+01:00',
         'timeZone': 'Europe/London'
-      },
-      'extendedProperties': {
-        'shared': {
-          'competitionName': 'The Maritime Cup',
-          'playedOnCourse': 'Dufferin'
-        }
       }
     }
-
-    /!*  Sample event created in Google Calendar
-     {
-     "kind":"calendar#event",
-     "etag":"\"2981454882834000\"",
-     "id":"oo3ucrbj28ipi9nbiob2cc89t4",
-     "status":"confirmed",
-     "htmlLink":"https://www.google.com/calendar/event?eid=b28zdWNyYmoyOGlwaTluYmlvYjJjYzg5dDQgYmRzNjA1MkBt",
-     "created":"2017-03-28T18:57:21.000Z",
-     "updated":"2017-03-28T18:57:21.417Z",
-     "location":"Clandeboye Golf Club, 51 Tower Rd, Bangor, Newtownards BT23, UK",
-     "creator": {
-     "email":"bds6052@gmail.com",
-     "displayName":"Brian Smith",
-     "self":true
-     },
-     "organizer": {
-     "email":"bds6052@gmail.com",
-     "displayName":"Brian Smith",
-     "self":true
-     },
-     "start": {
-     "dateTime":"2017-03-28T20:00:00+01:00",
-     "timeZone":"Europe/London"
-     },
-     "end": {
-     "dateTime":"2017-03-28T21:00:00+01:00",
-     "timeZone":"Europe/London"
-     },
-     "iCalUID":"oo3ucrbj28ipi9nbiob2cc89t4@google.com",
-     "sequence":0,
-     "reminders": {
-     "useDefault":true
-     }
-     }
-     *!/
 
     googleCalendar.events.insert(config.calendarId, addEventBody, function (addEventError, addEventResponse) {
       console.log('GOOGLE RESPONSE:', addEventError, addEventResponse)
 
       if (addEventError) {
+        console.log('[ERROR] %s:%s \n%s', addEventError.name, addEventError.msg, addEventError.message)
         res.send(400, addEventError)
       } else {
+
         console.log(addEventResponse)
 
-//                res.send(200, addEventResponse); // Google is throwing an error here
+        var data = {title: 'Golf Test Routines', pageName: 'Add google-calendar Competition Page'}
+        res.render('about', data)
       }
     })
-*/
-
-    res.render('about', { title: 'Golf Test Routines', pageName: 'Add google-calendar Competition Page' })
   })
 
   return router
